53f0446008cb6393dbc3a15d9a5a42f5cd31c256
EmLiu0314 <emilyliu52106@gmail.com>
2024-03-02 22:52:31 -0600

    Feb 26 Changes

--- TicTacToe/TicTacToev1/TicTacToev1/Models/GameModels.swift
+++ TicTacToe/TicTacToev1/TicTacToev1/Models/GameModels.swift
@@ -5,7 +5,7 @@
 //  Created by Liu, Emily on 2/19/24.
 //
 
-import Foundation
+import SwiftUI
 
 enum GameType {//a data structure with different properties
     case single, bot, peer, undetermined
@@ -27,4 +27,35 @@ enum GameType {//a data structure with different properties
         }
     }
     
+}
+
+enum GamePiece: String{
+    case x, o
+    var image:Image{
+        Image(self.rawValue)
+    }
+}
+
+struct Player{
+    let gamePiece:GamePiece
+    var name:String
+    var moves:[Int] = [] //keeps tracks of what the user clicks on
+    var isCurrent = false
+    
+    var isWinner:Bool{
+        for moves in Moves.winningMoves {
+            if moves.allSatisfy(self.moves.contains){ //self: whoever is active right now, whoever has control 
+                return true
+            }
+            
+        }
+        return false
+    }
+}
+
+enum Moves{
+    static var all = [1,2,3,4,5,6,7,8,9]
+    static var winningMoves = [[1,2,3],[4,5,6],[7,8,9],
+                        [1,4,7],[2,5,8],[3,6,9],
+                        [1,5,9],[3,5,7]]
 }
--- TicTacToe/TicTacToev1/TicTacToev1/StartView.swift
+++ TicTacToe/TicTacToev1/TicTacToev1/StartView.swift
@@ -17,58 +17,70 @@ struct StartView: View {
     @FocusState private var focus:Bool
     //for the keyboard focus we want the keyboard to disappear when thegame starts; set it to false in that case
     //this will be set to true when we are within the text view
+    @State private var startGame = false
     
     var body: some View {
-        VStack {
-            Picker("Select Game", selection: $gameType) {
-                Text("Select Game Type").tag(GameType.undetermined) //default displayed text
-                
-                Text("Two Sharing Device").tag(GameType.single)
-                Text("Challenge your device").tag(GameType.bot)
-                Text("Challenge a friend").tag(GameType.peer)
-                
-            }.padding()
-                .background(RoundedRectangle(cornerRadius:10,style: .continuous).stroke(style: StrokeStyle(lineWidth:2)))
+        NavigationStack {
+            VStack {
+                Picker("Select Game", selection: $gameType) {
+                    Text("Select Game Type").tag(GameType.undetermined) //default displayed text
+                    
+                    Text("Two Sharing Device").tag(GameType.single)
+                    Text("Challenge your device").tag(GameType.bot)
+                    Text("Challenge a friend").tag(GameType.peer)
+                    
+                }.padding()
+                    .background(RoundedRectangle(cornerRadius:10,style: .continuous).stroke(style: StrokeStyle(lineWidth:2)))
                 Text(gameType.description)
                     .padding()
-                            
-            
-            VStack{
-                switch gameType {
-                case .single:
-                    TextField("Your Name", text: $yourName)
-                    TextField("Opponent Name", text: $opponentName)
-                case .bot:
-                    TextField("Your Name", text: $yourName)
-                case .peer:
-                    EmptyView()
-                case .undetermined:
-                    EmptyView()
                 
-                }
-            } .padding()
-                .textFieldStyle(.roundedBorder)
-                .focused($focus)
-                .frame(width:350)
-            
-            if gameType != .peer{
-                Button("Start Game"){
-                    focus = false
-                }
-                .buttonStyle(
-                    .borderedProminent)
+                
+                VStack{ //depending on game type, show text
+                    switch gameType {
+                    case .single:
+                        TextField("Your Name", text: $yourName)
+                        TextField("Opponent Name", text: $opponentName)
+                    case .bot:
+                        TextField("Your Name", text: $yourName)
+                    case .peer:
+                        EmptyView()
+                    case .undetermined:
+                        EmptyView()
+                        
+                    }
+                } .padding()
+                    .textFieldStyle(.roundedBorder)
+                    .focused($focus)
+                    .frame(width:350)
+                
+                if gameType != .peer{ //can you click on start game
+                    Button("Start Game"){
+                        focus = false
+                        startGame.toggle()
+                    }
+                    .buttonStyle(
+                        .borderedProminent)
                     .disabled(
                         gameType == .undetermined ||
                         gameType == .bot && yourName.isEmpty ||
                         gameType == .single && (yourName.isEmpty || opponentName.isEmpty)
+                        //can also take youName.isEmpty $$ ...
                         
                     )
-                
-                Image ("LaunchScreen")
+                    
+                    Image ("LightModeWelcome")
+                }
+                Spacer()
             }
-            Spacer()
+            .padding()
+            .navigationTitle("Tic-Tac-Toe")
+            //once press, start game, new screen is covered
+            .fullScreenCover(isPresented: $startGame) {
+                //game view is the one you want to see when you start screen
+                GameView()
+                    
+            }
         }
-        .padding()
     }
 }
 
