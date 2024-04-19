7eb79292fb4426b8f63b435b0b622a824f715483
EmLiu0314 <emilyliu52106@gmail.com>
2024-04-19 15:19:20 -0500

    Firebase practice

---
+++ FirebaseDemoV1/FirebaseDemoV1/FirebaseDemoV1App.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseDemoV1App.swift
+//  FirebaseDemoV1
+//
+//  Created by Liu, Emily on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseDemoV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
